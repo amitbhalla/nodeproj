52417a185d2a91015c48f6f000138c6902a805a3
Amit Bhalla <amit9815@gmail.com>
2021-10-15 17:16:41 +0530

    GET/POST created

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 import express from 'express';
+import { response } from 'express';
+import fs from 'fs';
 
 //  Base config
 const PORT = 3000;
 const ADDR = '0.0.0.0';
 const app = express();
 
-//  Routs
-app.get('/', (req, res) => {
+// Middleware
+app.use(express.json());
+
+// Helper functions
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-sample.json'));
+
+//  Routes
+app.get('/api/v1/tours', (req, res) => {
   res.status(200);
-  res.send('Hello World!');
+  res.json({ status: 200, data: { tours } });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    './dev-data/data/tours-sample.json',
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201);
+      res.json({ status: 200, data: { newTour } });
+    }
+  );
 });
 
 //  Event loop
