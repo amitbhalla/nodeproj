8d2e4fd3399c587f2a9ba9356c5a2fd6a1193cdc
Amit Bhalla <amit9815@gmail.com>
2021-10-17 14:02:24 +0530

    User Routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import fs from 'fs';
+import morgan from 'morgan';
 
 //  Base config
 const PORT = 3000;
@@ -7,12 +8,15 @@ const ADDR = '0.0.0.0';
 const app = express();
 
 // Middleware
+app.use(morgan('dev'));
 app.use(express.json());
 
 // Helper functions
 const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-sample.json'));
+const users = JSON.parse(fs.readFileSync('./dev-data/data/users.json'));
 
 // Route functions
+// Tour
 const getAllTours = (req, res) => {
   res.status(200);
   res.json({ status: 200, data: { tours } });
@@ -90,13 +94,91 @@ const deleteTourById = (req, res) => {
   }
 };
 
-//  Routes
+// User
+const getAllUsers = (req, res) => {
+  res.status(200);
+  res.json({ status: 200, data: { users } });
+};
+
+const getUserById = (req, res) => {
+  const id = req.params.id;
+  const user = users.find((el) => el._id === id);
+
+  if (user) {
+    res.status(200);
+    res.json({ status: 200, data: { user } });
+  } else {
+    res.status(404);
+    res.json({ status: 404, data: 'No user with this id exists' });
+  }
+};
+
+const postNewUser = (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  fs.writeFile('./dev-data/data/users.json', JSON.stringify(users), (err) => {
+    res.status(201);
+    res.json({ status: 200, data: { newUser } });
+  });
+};
+
+const patchUserById = (req, res) => {
+  const id = req.params.id;
+  const user = users.find((el) => el._id === id);
+
+  if (user) {
+    const updatedUser = { ...user, ...req.body };
+    users[id] = updatedUser;
+
+    fs.writeFile('./dev-data/data/users.json', JSON.stringify(users), (err) => {
+      res.status(201);
+      res.json({ status: 201, data: { updatedUser } });
+    });
+  } else {
+    res.status(404);
+    res.json({ status: 404, data: 'No user with this id exists' });
+  }
+};
+
+const deleteUserById = (req, res) => {
+  const id = req.params.id;
+  const user = users.find((el) => el._id === id);
+
+  if (user) {
+    const newUsers = users.filter((el) => {
+      return el != user;
+    });
+
+    fs.writeFile(
+      './dev-data/data/users.json',
+      JSON.stringify(newUsers),
+      (err) => {
+        res.status(200);
+        res.json({ status: 200, data: { newUsers } });
+      }
+    );
+  } else {
+    res.status(404);
+    res.json({ status: 404, data: 'No user with this id exists' });
+  }
+};
+
+// Routes
+// Tour
 app.route('/api/v1/tours').get(getAllTours).post(postNewTour);
 app
   .route('/api/v1/tours/:id')
   .get(getTourById)
   .patch(patchTourById)
   .delete(deleteTourById);
+
+//  User
+app.route('/api/v1/users').get(getAllUsers).post(postNewUser);
+app
+  .route('/api/v1/users/:id')
+  .get(getUserById)
+  .patch(patchUserById)
+  .delete(deleteUserById);
 
 //  Event loop
 app.listen(PORT, ADDR, () => console.log(`listening to ${PORT} on ${ADDR}`));
